bd3a09dd5d64737fb4eadad879eae09acd4cc32d
Cody Speroff <cnsperoff@gmail.com>
2017-05-26 19:05:15 -0500

    converted babel-preset-react to new version

--- webpack.config.js
+++ webpack.config.js
@@ -10,11 +10,14 @@ module.exports = {
     },
     devtool: 'inline-source-map',
     module: {
-      loaders: [
-        {
-          test: /\.js$/,
-          exclude: /(node_modules)/,
-          loader: 'babel',
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                loader: 'babel',
+                query: {
+                    presets: ['react']
+                }
         },
       ]
     }
